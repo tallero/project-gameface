2c95eb31bd5f6ddb884a5bcbaf0b37c8bc71b78b
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-08-20 06:13:20 +0200

    let's see if 1.22.x counts as 1.22.0 for pip

--- .github/workflows/ci-build.sh
+++ .github/workflows/ci-build.sh
@@ -4,6 +4,19 @@ _sys="mingw64"
 _sys="ucrt64"
 _bin="/${_sys}/bin"
 
+_prepare() {
+  local _github _commit _pkg
+  _pkg="python-numpy"
+  _github="https://raw.githubusercontent.com"
+  _commit="7ffcd8f7ffa0bf161da4ff40ee9444eb382fa8e0"
+  _url="${_github}/msys2/MINGW-packages/${_commit}/mingw-w64-${_pkg}/PKGBUILD"
+  mkdir "${HOME}/${_pkg}"
+  cd "${HOME}/${_pkg}"
+  wget "${_url}"
+  makepkg
+  pacman -U "${_pkg}"*.pkg.tar* --noconfirm
+}
+
 _build() {
   "${_bin}/pip" install -u requirements.txt \
 	                --prefer-binary
