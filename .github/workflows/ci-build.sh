a5d2e6fd7e56a6347357e8ac6aa777365ab896b7
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-08-20 06:00:35 +0200

    let's see if we can explicitly request the numpy version we need (1.22.0), provided it's still in the repos

--- .github/workflows/ci-build.sh
+++ .github/workflows/ci-build.sh
@@ -5,17 +5,7 @@ _sys="ucrt64"
 _bin="/${_sys}/bin"
 
 _build() {
-  local _pkgs=(
-    flatbuffers==2.0.0
-    matplotlib==3.7.1
-    opencv-contrib-python==4.7.0.72
-    psutil==5.9.4
-    pyautogui==0.9.53
-    customtkinter==5.1.2
-    PyDirectInput==1.0.4
-    pywin32==306
-    mediapipe==0.9.3.0)
-  "${_bin}/pip" install "${_pkgs[@]}" \
+  "${_bin}/pip" install -u requirements.txt \
 	                --prefer-binary
 }
 
