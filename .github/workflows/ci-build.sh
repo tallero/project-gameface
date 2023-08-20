0f83a51e45fac0507463f4757b996fdbe9da46a4
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-08-20 05:41:38 +0200

    let's see if we can avoid building numpy altogether

--- .github/workflows/ci-build.sh
+++ .github/workflows/ci-build.sh
@@ -4,19 +4,24 @@ _sys="mingw64"
 _sys="ucrt64"
 _bin="/${_sys}/bin"
 
-_prepare() {
-  "pip" install pyinstaller
-}
-
 _build() {
-  "pip" install -r "requirements.txt"
+  local _pkgs=(
+    flatbuffers==2.0.0
+    matplotlib==3.7.1
+    opencv-contrib-python==4.7.0.72
+    psutil==5.9.4
+    pyautogui==0.9.53
+    customtkinter==5.1.2
+    PyDirectInput==1.0.4
+    pywin32==306
+    mediapipe==0.9.3.0)
+  "pip" install "${_pkgs[@]}"
 }
 
 _package() {
   "pyinstaller" "build.spec"
 }
 
-_prepare
 _build
 _package
 ls
