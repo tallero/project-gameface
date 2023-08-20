ce3e0f72b0b42fc06fd56e347247bcc75d6f5267
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-08-20 02:47:05 +0200

    install requirements before running pyinstaller

--- .github/workflows/ci-build.sh
+++ .github/workflows/ci-build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+_bin="/mingw64/bin"
+
 _prepare() {
-  pip install pyinstaller
+  "${_bin}/pip" install pyinstaller
 }
 
 _build() {
-  pyinstaller "build.spec"
+  "${_bin}/pip" install -r "requirements.txt"
+}
+
+_package() {
+  "${_bin}/pyinstaller" "build.spec"
 }
 
 _prepare
 _build
+_package
