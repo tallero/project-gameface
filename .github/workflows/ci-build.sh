5439257bbee576ab819a2307c097ea0db3e06471
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-08-21 17:45:55 +0200

    let's go toward trying to run pyinstaller by simply having the packages installed in the ci environment; add pymsgbox

--- .github/workflows/ci-build.sh
+++ .github/workflows/ci-build.sh
@@ -9,6 +9,35 @@ _set_prefix() {
    export MINGW_PACKAGE_PREFIX="mingw-w64-ucrt-x86_64" 
 }
 
+_setup_pymsgbox() {
+  local _commit \
+        _ns \
+	_pkg \
+	_pkgname \
+	_prefix \
+	_url \
+	_ver \
+	pkgname
+  _prefix="mingw-w64"
+  _pkg="pymsgbox"
+  _ver="1.0.9"
+  _pkgname="${_pkg}-${_ver}"
+  pkgname="${_prefix}-${_pkgname}"
+  _ns="mingw-aur"
+  _commit="bec1d5423f32a7b6ac2e0e24c2608f85b4634ca6"
+  _url="https://github.com/${_ns}/${pkgname}"
+  git clone "${_url}"
+  cd "${pkgname}"
+  git checkout "${_commit}"
+  makepkg-mingw --nocheck
+  pacman -Rdd "${MINGW_PACKAGE_PREFIX}-${_pkg}" \
+              "${MINGW_PACKAGE_PREFIX}-python-${_pkg}" \
+	      --noconfirm
+  pacman -U "${MINGW_PACKAGE_PREFIX}"*".pkg.tar"* \
+	    --noconfirm
+  cd ".."
+}
+
 _setup_numpy() {
   local _commit \
         _ns \
@@ -76,6 +105,7 @@ _package() {
 }
 
 _set_prefix
+_setup_pymsgbox
 _setup_numpy
 _setup_opencv
 # _build
